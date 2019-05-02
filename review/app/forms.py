35cda4d81c1e611be0f87e16a926086a52741699
AzarovDN <azarovdn@icloud.com>
2019-05-02 22:52:02 +1000

    Оставил проверку валидации формы, выдает ошибку, когда форма не проходит валидацию. Как сделать не знаю.

--- review/app/forms.py
+++ review/app/forms.py
@@ -5,11 +5,11 @@ from .models import Review
 class ReviewForm(forms.ModelForm):
     text = forms.CharField(widget=forms.Textarea, label='Отзыв')
 
-    # def clean_text(self):
-    #     text = self.cleaned_data['text']
-    #     if len(text) < 10:
-    #         raise forms.ValidationError('Отзыв не может быть меньше 10 символов')
-    #     return text
+    def clean_text(self):
+        text = self.cleaned_data['text']
+        if len(text) < 10:
+            raise forms.ValidationError('Отзыв не может быть меньше 10 символов')
+        return text
 
     class Meta(object):
         model = Review
--- review/app/views.py
+++ review/app/views.py
@@ -33,35 +33,36 @@ class ProductView(DetailView):
         return context
 
 
-    def post(self, request, **kwargs):
-        form = ReviewForm(request.POST or None)
-        pk = kwargs['pk']
-        if form.is_valid():
-            text = request.POST['text']
-            product = Product.objects.get(id=pk)
-            Review.objects.create(text=text, product=product)
-        # else:
-        #     print(request)
-        #     return render(request, 'app/product_detail.html', {'form': form})
-
-        return redirect('product_detail', pk=pk)
-
-    # def post(self, request, *args, **kwargs):
-    #     form = ReviewForm(self.request.POST or None)
+    # def post(self, request, **kwargs):
+    #     form = ReviewForm(request.POST or None)
     #     pk = kwargs['pk']
-    #
     #     if form.is_valid():
     #         text = request.POST['text']
     #         product = Product.objects.get(id=pk)
     #         Review.objects.create(text=text, product=product)
     #     else:
-    #         return render(request, 'app/product_detail.html')
     #
-    #     if 'reviewed_products' in request.session:
-    #         value = request.session['reviewed_products']
-    #         value.append(kwargs['pk'])
-    #         request.session['reviewed_products'] = value
-    #     else:
-    #         request.session['reviewed_products'] = [pk]
+    #         return render(request, 'app/product_detail.html', {'form': form})
     #
     #     return redirect('product_detail', pk=pk)
+
+
+    def post(self, request, **kwargs):
+        form = ReviewForm(self.request.POST or None)
+        pk = kwargs['pk']
+
+        if form.is_valid():
+            text = request.POST['text']
+            product = Product.objects.get(id=pk)
+            Review.objects.create(text=text, product=product)
+        else:
+            return render(request, 'app/product_detail.html', {'form': form})
+
+        if 'reviewed_products' in request.session:
+            value = request.session['reviewed_products']
+            value.append(kwargs['pk'])
+            request.session['reviewed_products'] = value
+        else:
+            request.session['reviewed_products'] = [pk]
+
+        return redirect('product_detail', pk=pk)
